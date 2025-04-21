a46131477297afbc079110f1e4882fafcc07f333
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-21 20:23:16 +0100

    Log math expression with bracket notations and start on variables

--- Assignment-1/javascript.js
+++ Assignment-1/javascript.js
@@ -3,3 +3,10 @@ console.log(23 + 97);
 
 // Add six numbers together
 console.log(45 + 53 + 99 + 12 + 450 + 222);
+
+// Log math expression using bracket notations
+console.log((4 + 6 + 9) / 77);
+
+// Let's use some variables
+let a = 10;
+console.log(a);
