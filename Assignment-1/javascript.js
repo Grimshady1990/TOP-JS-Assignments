14e87a955deed1572cd0c56456da23a783380973
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-21 20:32:32 +0100

    Create a simple sequence to find the percentage

--- Assignment-1/javascript.js
+++ Assignment-1/javascript.js
@@ -15,6 +15,14 @@ console.log(a);
 a = 55;
 console.log(a);
 
+// Try this sequence
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
 
 
 
