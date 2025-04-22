a1f5e169f71cdfc79974cb398a0852da31d30683
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-22 06:17:20 +0100

    comment out code and add new task

--- Assignment-1/javascript.js
+++ Assignment-1/javascript.js
@@ -8,12 +8,12 @@ console.log(45 + 53 + 99 + 12 + 450 + 222);
 console.log((4 + 6 + 9) / 77);
 
 // Let's use some variables
-let a = 10;
-console.log(a);
+//let a = 10;
+//console.log(a);
 
 // log "a" with a different value
-a = 55;
-console.log(a);
+//a = 55;
+//console.log(a);
 
 // Try this sequence
 const max = 57;
@@ -27,6 +27,30 @@ console.log(percentage);
 
 
 // Add "b" to the bottom of the script
-let b = 7 * a;
-console.log(b);
+//let b = 7 * a;
+//console.log(b);
+/*
+// What are the results
+
+"" + 1 + 0 // "10"
+"" - 1 + 0 // -1
+true + false // 1
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // "9px"
+"$" + 4 + 5 // "$45"
+"4" - 2 // 2
+"4px" - 2 // NaN
+"  -9  " + 5 // "  -9  5"
+"  -9  " - 5 // -14
+null + 1 // 1
+undefined + 1 // NaN
+" \t \n" - 2 // -2
+*/
+// Fix the addition
+
+let a = +prompt("First Number", 1);
+let b = +prompt("Second Number", 2);
+
+alert(a + b);
 
