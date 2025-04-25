c9a3d5e76140657e1f1dd3da110b82acab2e071f
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-25 03:57:56 +0100

    Add second function

--- Assignment-2/javascript.js
+++ Assignment-2/javascript.js
@@ -1,5 +1,6 @@
 // Assignment 2 javascript
 
+// Question 1: Add 7 to chosen number
 function add7(num) {
     return num + 7;
     
@@ -7,3 +8,17 @@ function add7(num) {
 
 console.log(add7(10));
 console.log(add7(3));
+
+
+//Question 2: Take 2 numbers and multiply them
+
+function multiply(a, b) {
+    return a * b; 
+}
+
+console.log(multiply(10, 9));
+console.log(multiply(6, 6));
+
+// Question 3: returns string with first letter capitalized
+
+function capitalize()
