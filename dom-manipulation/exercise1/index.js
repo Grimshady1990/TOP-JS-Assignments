8858a8963930fe1bba3da9d7cc986b291eee3d2f
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-07 22:09:02 +0100

    Add html content through the DOM

--- dom-manipulation/exercise1/index.js
+++ dom-manipulation/exercise1/index.js
@@ -1,1 +1,16 @@
-console.log("Suckerr!!");
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+let pTag = document.createElement("p");
+
+pTag.textContent = "Hey I'm red!";
+
+pTag.style.color = "red";
+
+container.appendChild(content);
+container.appendChild(pTag);
+
+
+
