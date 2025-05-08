54592b72c6503a84be83bffb4b527876f0faed4c
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-08 05:20:03 +0100

    Complete exercise

--- dom-manipulation/exercise1/index.js
+++ dom-manipulation/exercise1/index.js
@@ -9,8 +9,26 @@ pTag.textContent = "Hey I'm red!";
 
 pTag.style.color = "red";
 
+const content2 = document.createElement("div");
+content2.setAttribute("style", "background-color: pink; border-style: solid");
+content2.innerHTML = "<h1> I'm in a div </h1> <p> ME TOO! </p>";
+
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+
+const btn3 = document.querySelector("#btn3");
+
+btn3.addEventListener("click", () => {
+    alert("Hello World");
+});
+
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+});
+
 container.appendChild(content);
 container.appendChild(pTag);
+container.appendChild(content2);
 
 
 
